98b728f1228ce53743928a9e14056da7fbd572c5
djurosarac <38404864+djurosarac@users.noreply.github.com>
2019-09-30 00:19:26 +0200

    aaa

--- PPPprodavnica1/Controllers/AccountController.cs
+++ PPPprodavnica1/Controllers/AccountController.cs
@@ -12,7 +12,7 @@ using PPPprodavnica1.Models;
 
 namespace PPPprodavnica1.Controllers
 {
-    [Authorize]
+    [Authorize]//
     public class AccountController : Controller
     {
         private ApplicationSignInManager _signInManager;
